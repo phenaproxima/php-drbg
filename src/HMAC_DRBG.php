0fed1fc29fe9006d600c8f6232b222673b443a76
Phéna Proxima <phen@Proxima-Corona.local>
2015-10-19 20:36:26 -0400

    Documented all methods of HMAC_DRBG.

--- src/HMAC_DRBG.php
+++ src/HMAC_DRBG.php
@@ -5,6 +5,8 @@ namespace phenaproxima;
 class HMAC_DRBG {
 
   /**
+   * The hash strength. Can be 112, 128, 192, or 256.
+   *
    * @var int
    */
   protected $strength;
@@ -24,6 +26,21 @@ class HMAC_DRBG {
    */
   protected $reseedCount = 1;
 
+  /**
+   * Constructs an HMAC DRBG.
+   *
+   * @param string $entropy
+   *   Binary data used for entropy.
+   * @param int $strength
+   *   (optional) Hash strength. Can be 112-256.
+   * @param string $personalizer
+   *   (optional) Additional binary data to combine with the entropy data.
+   *
+   * @throws \OutOfBoundsException if $strength is greater than 256.
+   * @throws \LengthException if $personalizer is longer than 256 bits.
+   * @throws \LengthException if entropy is shorter than (1.5 * $strength) bits.
+   * @throws \LengthException if $entropy is longer than 1000 bits.
+   */
   public function __construct($entropy, $strength = 256, $personalizer = '') {
     if ($strength > 256) {
       throw new \OutOfBoundsException('Strength cannot exceed 256 bits.');
@@ -57,11 +74,27 @@ class HMAC_DRBG {
     $this->update($entropy . $personalizer);
   }
 
+  /**
+   * Creates an HMAC hash.
+   *
+   * @param ByteArray $key
+   *   The binary hash key, wrapped in a ByteArray.
+   * @param ByteArray $data
+   *   The binary data to hash, wrapped in a ByteArray.
+   *
+   * @return ByteArray
+   */
   protected function HMAC(ByteArray $key, ByteArray $data) {
     $hash = hash_hmac('sha256', $data->toBinaryString(), $key->toBinaryString(), TRUE);
     return ByteArray::create($hash);
   }
 
+  /**
+   * Updates the internal state of the DRBG.
+   *
+   * @param string|null $data
+   *   (optional) Binary data to update with.
+   */
   protected function update($data = NULL) {
     $this->k = $this->HMAC($this->k, $this->v->copy()->append([0x0], $data));
     $this->v = $this->HMAC($this->k, $this->v);
@@ -72,6 +105,15 @@ class HMAC_DRBG {
     }
   }
 
+  /**
+   * Reseeds the DRBG.
+   *
+   * @param string $entropy
+   *   Binary data used for entropy.
+   *
+   * @throws \LengthException if $entropy is shorter than the configured hash
+   * strength, or longer than 1000 bits.
+   */
   public function reseed($entropy) {
     if ((strlen($entropy) * 8) < $this->strength) {
       throw new \LengthException('Entropy must be at least ' . $this->strength . ' bits.');
@@ -84,6 +126,21 @@ class HMAC_DRBG {
     $this->reseedCount = 1;
   }
 
+  /**
+   * Generates a string of random bits.
+   *
+   * @param string $length
+   *   Length of the data to generate, in bytes. Returned data will be
+   *   ($length * 2) bytes.
+   * @param int $strength
+   *   (optional) Hash strength; defaults to 256 bits. Not currently used.
+   *
+   * @return ByteArray
+   *
+   * @throws \InvalidArgumentException if $length is greater than 7500 bits.
+   * @throws \OutOfBoundsException if $strength is greater than the configured
+   * hash strength.
+   */
   public function generate($length, $strength = 256) {
     if ($length * 8 > 7500) {
       throw new \InvalidArgumentException('Cannot generate more than 7500 bits in a single call.');
